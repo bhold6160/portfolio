22dbaf874da274fac950492a05f89f2044cc1b11
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-12 12:00:59 -0700

    added objects.js and added setter methods to app.js: not functioning

--- script/app.js
+++ script/app.js
@@ -3,8 +3,28 @@
 var resumeArr = [];
 
 //Constructor function
-function Resume(name, place, date) {
-  this.name
-  this.place
-  this.date
+function Resume(resumeDataObj) {
+  this.name = resumeDataObj.name;
+  this.location = resumeDataObj.location;
+  this.dates = resumeDataObj.dates;
+  this.position = resumeDataObj.position;
+  this.description = resumeDataObj.description;
 }
+
+Resume.prototype.aboutMe = function() {
+var newAbout = $('article.template').clone();
+
+newAbout.find('h1').html(this.name);
+newAbout.find('#locationId').html(this.location);
+newAbout.find('#datesId').html(this.dates);
+newAbout.find('#positionId').html(this.position);
+newAbout.find('article-body').html(this.description);
+}
+
+resumeRawData.forEach(function(resumeObject){
+  resumeArr.push(new Resume(resumeObject));
+});
+
+resumeArr.forEach(function(resume) {
+  $('.data').append(resume.aboutMe());
+});
