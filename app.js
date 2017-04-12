e3f45ded0182ceb4db754d2a880958f9c152f0c2
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-12 10:42:19 -0700

    working on getting nav icons to function properly

--- app.js
+++
@@ -1,6 +0,0 @@
-//Constructor function
-function Resume(name, place, date, ) {
-  this.name
-  this.place
-  this.date
-}
---
+++ script/app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var resumeArr = [];
+
+//Constructor function
+function Resume(name, place, date) {
+  this.name
+  this.place
+  this.date
+}
