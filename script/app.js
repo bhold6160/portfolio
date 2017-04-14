9efa5c5dd9f73b29afc61d0549d203a38fa52d75
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-13 17:41:00 -0700

    css changes rounded picture and placed data side by side

--- script/app.js
+++ script/app.js
@@ -1,32 +1,54 @@
 'use strict'
 
 var resumeArr = [];
+var schoolArr = [];
+var view = {};
 
 //Constructor function
-function Resume(resumeDataObj) {
-  this.name = resumeDataObj.name;
-  this.location = resumeDataObj.location;
-  this.dates = resumeDataObj.dates;
-  this.position = resumeDataObj.position;
-  this.description = resumeDataObj.description;
-}
+function Resume(dataOb) {
+  this.name = dataOb.name;
+  this.location = dataOb.location;
+  this.dates = dataOb.dates;
+  this.position = dataOb.position;
+  this.description = dataOb.description;
+};
 
-Resume.prototype.aboutMe = function() {
-var newAbout = $('.data').clone();
-newAbout.removeClass('data');
+Resume.prototype.aboutMe = function () {
+  var newAbout = $('.data').clone();
+  newAbout.removeClass('data');
+  newAbout.find('h1').html(this.name);
+  newAbout.find('#locationId').html(this.location);
+  newAbout.find('#datesId').html(this.dates);
+  newAbout.find('#positionId').html(this.position);
+  newAbout.find('.article-body').html(this.description);
+  return newAbout;
+};
 
-newAbout.find('h1').html(this.name);
-newAbout.find('#locationId').html(this.location);
-newAbout.find('#datesId').html(this.dates);
-newAbout.find('#positionId').html(this.position);
-newAbout.find('.article-body').html(this.description);
-return newAbout;
-}
-
-resumeRawData.forEach(function(resumeObject){
+resumeRawData.forEach(function (resumeObject) {
   resumeArr.push(new Resume(resumeObject));
 });
 
-resumeArr.forEach(function(resume) {
+resumeArr.forEach(function (resume) {
   $('#aboutData').append(resume.aboutMe());
+});
+
+schoolRawData.forEach(function (schoolObject) {
+  schoolArr.push(new Resume(schoolObject));
+});
+
+schoolArr.forEach(function (school) {
+  $('#schoolData').append(school.aboutMe());
+});
+
+view.handleNav = function () {
+  $('.top-nav .tab').on('click', function () {
+    $('.tab-content').hide();
+    $('.tab-content[id="' + $(this).attr('data-content') + '"]').show();
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
+$(document).ready(function () {
+  view.handleNav();
 });
