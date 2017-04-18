e9866e305401b89a2beffc0be50ff08206137d0a
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-18 13:01:47 -0700

    added constructor and social media pages

---
+++ script/app.js
@@ -0,0 +1,64 @@
+'use strict'
+
+var resumeArr = [];
+var schoolArr = [];
+var view = {};
+
+//Constructor function
+function Resume(dataObj) {
+  this.title = dataObj.title;
+  this.name = dataObj.name;
+  this.location = dataObj.location;
+  this.dates = dataObj.dates;
+  this.position = dataObj.position;
+  this.description = dataObj.description;
+  this.lorem = dataObj.lorem;
+};
+
+function School(dataObj) {
+  this.title = dataObj.title;
+  this.name = dataObj.name;
+  this.location = dataObj.location;
+  this.dates = dataObj.dates;
+}
+
+Resume.prototype.aboutMe = function () {
+  var aboutSource = $('#template-resume').html();
+  var aboutTemplate = Handlebars.compile(aboutSource);
+  return aboutTemplate(this);
+};
+
+School.prototype.aboutMe = function () {
+  var aboutSource = $('#template-school').html();
+  var aboutTemplate = Handlebars.compile(aboutSource);
+  return aboutTemplate(this);
+};
+
+resumeRawData.forEach(function (resumeObject) {
+  resumeArr.push(new Resume(resumeObject));
+});
+
+resumeArr.forEach(function (resume) {
+  $('#aboutData').append(resume.aboutMe());
+});
+
+schoolRawData.forEach(function (schoolObject) {
+  schoolArr.push(new School(schoolObject));
+});
+
+schoolArr.forEach(function (school) {
+  $('#schoolData').append(school.aboutMe());
+});
+
+view.handleNav = function () {
+  $('.top-nav .tab').on('click', function () {
+    $('.tab-content').hide();
+    $('.tab-content[id="' + $(this).attr('data-content') + '"]').fadeIn(1500);
+  });
+
+  $('.top-nav .tab:first').click();
+};
+
+$(document).ready(function () {
+  view.handleNav();
+});
---
+++ script/objects.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var resumeRawData = [
+  {
+    name: 'Catus Madison Park',
+    location: 'Seattle, WA',
+    dates: '3/2016 - Present',
+    position: 'Server',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    name: 'Dukes Chowder House',
+    location: 'Tukwilla, WA',
+    dates: '3/2014 - 1/2016',
+    position: 'Server',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    name: 'Prima Margherita',
+    location: 'Des Moines, WA',
+    dates: '5/2013 - 6/2014',
+    position: 'Server',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+];
+
+var schoolRawData = [
+  {
+    name: 'Code Fellows',
+    location: 'Seattle, WA',
+    dates: '2017 - Present',
+  },
+  {
+    name: 'Central Washington University',
+    location: 'Ellensburg, WA',
+    dates: '2011-2013',
+  },
+  {
+    name: 'Highline Community College',
+    location: 'Des Moines, WA',
+    dates: '2007-2011',
+  },
+];
