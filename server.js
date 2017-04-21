9ecd576789fd673da7b3f4c9b1740dcf3e367356
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-21 09:25:57 -0700

    minor changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const client = new pg.Client(conString);
 client.connect();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('.'));
 
 app.listen(PORT, function () {
