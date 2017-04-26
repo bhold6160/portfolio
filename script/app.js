c93917717e6937f8e479fba31e7fbdc589615b5f
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-26 11:33:15 -0700

    wrapped js file in an IIFE

--- script/app.js
+++ script/app.js
@@ -1,46 +1,47 @@
 'use strict'
+(function(module) {
 
-// $('.header').css('background-image', 'url(images/skyline-resize.jpg)');
-let resumeArr = [];
-let schoolArr = [];
-const view = {};
+  // $('.header').css('background-image', 'url(images/skyline-resize.jpg)');
+  let resumeArr = [];
+  let schoolArr = [];
+  const view = {};
 
-function Resume(dataObj) {
-  this.name = dataObj.name;
-  this.location = dataObj.location;
-  this.dates = dataObj.dates;
-  this.position = dataObj.position;
-  this.description = dataObj.description;
-  this.category = dataObj.category;
-};
+  function Resume(dataObj) {
+    this.name = dataObj.name;
+    this.location = dataObj.location;
+    this.dates = dataObj.dates;
+    this.position = dataObj.position;
+    this.description = dataObj.description;
+    this.category = dataObj.category;
+  };
 
-function School(dataObj) {
-  this.name = dataObj.name;
-  this.location = dataObj.location;
-  this.dates = dataObj.dates;
-  this.category = dataObj.category;
-}
+  function School(dataObj) {
+    this.name = dataObj.name;
+    this.location = dataObj.location;
+    this.dates = dataObj.dates;
+    this.category = dataObj.category;
+  }
 
-Resume.prototype.aboutMe = function () {
-  let aboutSource = $('#template-resume').html();
-  let aboutTemplate = Handlebars.compile(aboutSource);
-  return aboutTemplate(this);
-};
+  Resume.prototype.aboutMe = function () {
+    let aboutSource = $('#template-resume').html();
+    let aboutTemplate = Handlebars.compile(aboutSource);
+    return aboutTemplate(this);
+  };
 
-School.prototype.aboutMe = function () {
-  let aboutSource = $('#template-school').html();
-  let aboutTemplate = Handlebars.compile(aboutSource);
-  return aboutTemplate(this);
-};
+  School.prototype.aboutMe = function () {
+    let aboutSource = $('#template-school').html();
+    let aboutTemplate = Handlebars.compile(aboutSource);
+    return aboutTemplate(this);
+  };
 
-view.handleNav = function () {
-  $('.top-nav .tab').on('click', function () {
-    $('.tab-content').hide();
-    $('.tab-content[id="' + $(this).attr('data-content') + '"]').fadeIn(1500);
-  });
-};
+  view.handleNav = function () {
+    $('.top-nav .tab').on('click', function () {
+      $('.tab-content').hide();
+      $('.tab-content[id="' + $(this).attr('data-content') + '"]').fadeIn(1500);
+    });
+  };
 
-view.initIndexPage = function (rawDataObj) {
+  view.initIndexPage = function (rawDataObj) {
     rawDataObj.forEach(function (resumeObject) {
       resumeArr.push(new Resume(resumeObject));
     });
@@ -62,19 +63,20 @@ view.initIndexPage = function (rawDataObj) {
     });
   };
 
-$(document).ready(function () {
+  $(document).ready(function () {
     view.handleNav();
     Resume.fetchAll();
     $('.top-nav .tab:first').click();
   });
 
-Resume.fetchAll = function () {
-  if (localStorage.rawDataObj) {
-    view.initIndexPage(JSON.parse(localStorage.rawDataObj));
-  } else {
-    $.getJSON('data/objects.json').then(rawDataObj => {
-      view.initIndexPage(rawDataObj);
-      localStorage.setItem('rawDataObj', JSON.stringify(rawDataObj));
-    });
-  }
-};
+  Resume.fetchAll = function () {
+    if (localStorage.rawDataObj) {
+      view.initIndexPage(JSON.parse(localStorage.rawDataObj));
+    } else {
+      $.getJSON('data/objects.json').then(rawDataObj => {
+        view.initIndexPage(rawDataObj);
+        localStorage.setItem('rawDataObj', JSON.stringify(rawDataObj));
+      });
+    }
+  };
+}(window));
